ed574913fc03cdb633ae1d1049c0f9d6ca917b50
abhi-shek-11 <abhi3832@gmail.com>
2024-05-20 00:30:58 +0530

    updated port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, host: true, strictPort: true }
+  server: { port: 8000, host: true, strictPort: true },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
